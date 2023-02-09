cf14e6de31b860ccaae1b1586d4d334b8271f999
Marine <113897681+MarineThms@users.noreply.github.com>
2023-02-09 23:23:33 +0100

    Add files via upload

---
+++ ButtonApp/ButtonApp/ButtonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonAppApp.swift
+//  ButtonApp
+//
+//  Created by Apprenant 07 on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ButtonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
